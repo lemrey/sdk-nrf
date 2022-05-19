48c5c69c7137994c462641b7ede3278228e8777a
Emil Obalski <emil.obalski@nordicsemi.no>
2022-05-19 11:20:00 +0200

    samples: multicore: Prepare hello_world for nrf54fpga
    Add configurtion for nrf54fpga board.
    Adapt build system to allow to build for RiscV.
    
    Jira: NCSDK-15182.
    
    Signed-off-by: Emil Obalski <emil.obalski@nordicsemi.no>

--- samples/multicore/hello_world/README.rst
+++ samples/multicore/hello_world/README.rst
@@ -17,7 +17,7 @@ The sample supports the following development kits:
 
 .. table-from-rows:: /includes/sample_board_rows.txt
    :header: heading
-   :rows: nrf5340dk_nrf5340_cpuapp
+   :rows: nrf5340dk_nrf5340_cpuapp, nrf54fpga_nrf5420_cpuapp
 
 Overview
 ********
--- samples/multicore/hello_world/src/main.c
+++ samples/multicore/hello_world/src/main.c
@@ -7,8 +7,19 @@
 #include <zephyr.h>
 #include <sys/printk.h>
 
+#ifdef CONFIG_BOARD_NRF54FPGA_NRF5420_CPUAPP
+#define PPR_START_ADDR 0x1E0A0000
+#include <hal/nrf_vpr.h>
+#endif
+
 int main(void)
 {
+#ifdef CONFIG_BOARD_NRF54FPGA_NRF5420_CPUAPP
+	/* Enable PPr core */
+	nrf_vpr_initpc_set(NRF_VPR130, PPR_START_ADDR);
+	nrf_vpr_cpurun_set(NRF_VPR130, true);
+#endif
+
 	printk("Hello world from %s\n", CONFIG_BOARD);
 
 	return 0;
