6af06d5aa1953f90275d6b1636c19c1ce9f9d905
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2022-07-13 14:52:06 +0200

    cmake: corefw: Update board names and align UICR generation
    UICRs are generated by secdom now. There is no need for
    an explicit call to regtool.
    
    Add and update board names.
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

--- cmake/core_fw.cmake
+++ cmake/core_fw.cmake
@@ -5,9 +5,30 @@
 #
 
 function(build_secdom)
-  if (CONFIG_SOC_SERIES_NRF54X)
     include(ExternalProject)
-    set(BOARD_REMOTE "nrf54fpga_nrf5420_cpusec")
+
+    # Select a matching secdom board.
+    if(CONFIG_SOC_NRF5420)
+      if(CONFIG_EMULATOR_FPGA)
+        set(BOARD_REMOTE "nrf54fpga_nrf5420_cpusec")
+      elseif(CONFIG_EMULATOR_Z1)
+        set(BOARD_REMOTE "nrf54emu_nrf5420_cpusec")
+      endif()
+    elseif(CONFIG_SOC_NRF5420_SOC1)
+      if(CONFIG_EMULATOR_FPGA)
+        set(BOARD_REMOTE "nrf54fpga_nrf5420soc1_cpusec")
+      elseif(CONFIG_EMULATOR_Z1)
+        set(BOARD_REMOTE "nrf54emu_nrf5420soc1_cpusec")
+      endif()
+    elseif(CONFIG_SOC_NRF9230)
+      if(CONFIG_EMULATOR_Z1)
+        set(BOARD_REMOTE "nrf92emu_nrf9230_cpusec")
+      endif()
+    endif()
+
+    if (NOT DEFINED BOARD_REMOTE)
+      message(FATAL_ERROR "SDFW cannot be built for this SoC")
+    endif()
 
     ExternalProject_Add(
       secdom
@@ -18,15 +39,29 @@ function(build_secdom)
         -DCONFIG_RESET_ON_FATAL_ERROR:STRING=n
       BUILD_ALWAYS True
     )
-  else()
-    message(FATAL_ERROR "Incorrect SoC series")
-  endif()
 endfunction()
 
 function(build_sysctrl)
-  if (CONFIG_SOC_SERIES_NRF54X)
     include(ExternalProject)
-    set(BOARD_REMOTE "nrf54sysc_nrf5420_cpusys")
+
+    # Select a matching sysctrl board.
+    if(CONFIG_SOC_NRF5420)
+      if(CONFIG_EMULATOR_FPGA)
+        set(BOARD_REMOTE "nrf54fpga_nrf5420_cpusys")
+      elseif(CONFIG_EMULATOR_Z1)
+        set(BOARD_REMOTE "nrf54emu_nrf5420_cpusys")
+      endif()
+    elseif(CONFIG_SOC_NRF5420_SOC1)
+      if(CONFIG_EMULATOR_FPGA)
+        set(BOARD_REMOTE "nrf54fpga_nrf5420soc1_cpusys")
+      elseif(CONFIG_EMULATOR_Z1)
+        set(BOARD_REMOTE "nrf54emu_nrf5420soc1_cpusys")
+      endif()
+    endif()
+
+    if (NOT DEFINED BOARD_REMOTE)
+      message(FATAL_ERROR "SCFW cannot be built for this SoC")
+    endif()
 
     ExternalProject_Add(
       sysctrl
@@ -34,37 +69,7 @@ function(build_sysctrl)
       INSTALL_COMMAND ""
       CMAKE_CACHE_ARGS
         -DBOARD:STRING=${BOARD_REMOTE}
+        -DBUILD_TYPE:STRING=Release
       BUILD_ALWAYS True
     )
-  else()
-    message(FATAL_ERROR "Incorrect SoC series")
-  endif()
-endfunction()
-
-function(create_app_uicr)
-  # Adds an uicr to the build.
-  #
-  # TODO: Add DOMAIN depandent generation.
-  #       Right now UICR_CONF_FILE is ment to be used for App core.
-  #       Domain could be obtained from ${BOARD}
-
-  if (DEFINED UICR_CONF_FILE AND EXISTS UICR_CONF_FILE)
-    set(UICR_CONF_FILE ${UICR_CONF_FILE})
-  elseif (EXISTS ${APPLICATION_CONFIG_DIR}/uicr/${BOARD}.yaml)
-    set(UICR_CONF_FILE ${APPLICATION_CONFIG_DIR}/uicr/${BOARD}.yaml)
-  else()
-    message(FATAL_ERROR "UICR file does not exists")
-  endif()
-
-  set(DOMAIN "application")
-
-  nrf_regtool_generate_icr_from_yaml(
-    CONFIG_FILE_IN ${UICR_CONF_FILE}
-    HEX_FILE_NAME_PREFIX app_uicr
-    TARGET_DOMAIN ${DOMAIN}
-    HEX_FILE_OUT uicr_hex_out
-    ICR_TYPE UICR
-  )
-  add_custom_target(uicr_target ALL DEPENDS ${uicr_hex_out})
-
 endfunction()
