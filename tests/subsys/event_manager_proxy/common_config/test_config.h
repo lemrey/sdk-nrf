a64605aac4fca44941b4f35d675ad19e2881238e
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2022-09-13 16:45:17 +0200

    tests: event_manager_proxy: Extend timouts to pass on slower cores
    Allow slower FPGA cores more time.
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

--- tests/subsys/event_manager_proxy/common_config/test_config.h
+++ tests/subsys/event_manager_proxy/common_config/test_config.h
@@ -4,11 +4,25 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#define TEST_CONFIG_SIMPLE_BURST_SIZE 10000
-#define TEST_CONFIG_DATA_BURST_SIZE 10000
-#define TEST_CONFIG_DATA_BIG_BURST_SIZE 1000
+#if defined(BOARD_FPGA)
+	/* FPGA boards are slower and executing full scale test is impractical. */
+	#define TEST_CONFIG_SIMPLE_BURST_SIZE 100
+	#define TEST_CONFIG_DATA_BURST_SIZE 100
+	#define TEST_CONFIG_DATA_BIG_BURST_SIZE 10
+#else
+	#define TEST_CONFIG_SIMPLE_BURST_SIZE 10000
+	#define TEST_CONFIG_DATA_BURST_SIZE 10000
+	#define TEST_CONFIG_DATA_BIG_BURST_SIZE 1000
+#endif
 
 /* Test related timeouts. */
-#define TEST_TIMEOUT_BASE_S 1
-#define RESPONSE_TIMEOUT_S 1
-#define REMOTE_START_TIMEOUT_S 1
+#if defined(BOARD_FPGA)
+	/* FPGA boards need more time. */
+	#define TEST_TIMEOUT_BASE_S 10
+	#define RESPONSE_TIMEOUT_S 10
+	#define REMOTE_START_TIMEOUT_S 10
+#else
+	#define TEST_TIMEOUT_BASE_S 1
+	#define RESPONSE_TIMEOUT_S 1
+	#define REMOTE_START_TIMEOUT_S 1
+#endif
