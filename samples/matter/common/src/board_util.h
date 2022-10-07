cfe5b39f5ad1dade96ca0f6c835498bde8b82189
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2022-10-07 10:09:19 +0200

    [nrf noup] Aligned the Master Lock FPGA during Haltium update
    During Haltium ncs update 220926, there were several alignments
    needed to run Matter Lock Sample on the FPGA board.
    
    Signed-off-by: Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>

--- samples/matter/common/src/board_util.h
+++ samples/matter/common/src/board_util.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#if !defined(CONFIG_EMULATOR_FPGA)
+
 #include <zephyr/devicetree.h>
 
 #define LEDS_NODE_ID DT_PATH(leds)
@@ -13,3 +15,5 @@
 #define INCREMENT_BY_ONE(button_or_led) +1
 #define NUMBER_OF_LEDS (0 DT_FOREACH_CHILD(LEDS_NODE_ID, INCREMENT_BY_ONE))
 #define NUMBER_OF_BUTTONS (0 DT_FOREACH_CHILD(BUTTONS_NODE_ID, INCREMENT_BY_ONE))
+
+#endif
