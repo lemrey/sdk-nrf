d8d28dead965f9c52180525d5d4487d2b5209ea4
Rafal Dyla <rafal.dyla@nordicsemi.no>
2022-10-14 15:28:58 +0200

    [nrf noup] tests: Restore fake event manager
    Restored fake event manager from fast path
    
    Signed-off-by: Rafal Dyla <rafal.dyla@nordicsemi.no>

---
+++ tests/unity/fake_event_manager/event_manager.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include "event_manager.h"
+
+void _event_submit(struct event_header *eh)
+{
+
+}
+
+int event_manager_init(void)
+{
+	return 0;
+}
