87e4ad0b590d18cef00990f7539641f040a4382a
Emil Obalski <Emil.Obalski@nordicsemi.no>
2022-12-08 12:32:55 +0100

    applications: machine_learning: nRF54h20dk enable PPR
    Enable ppr core and sample with ppr. Data read by cpuppr
    are transferred to cpuapp where are prcessed by neural
    network and outputs recognized wave.
    
    Jira: NCSDK-17337.
    
    Signed-off-by: Emil Obalski <Emil.Obalski@nordicsemi.no>

--- applications/machine_learning/src/main.c
+++ applications/machine_learning/src/main.c
@@ -12,9 +12,19 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(MODULE);
 
+#if defined(CONFIG_BOARD_NRF54H20DK_NRF54H20_CPUAPP) && defined(CONFIG_ML_APP_INCLUDE_REMOTE_IMAGE)
+#define PPR_START_ADDR DT_REG_ADDR(DT_NODELABEL(ppr_code))
+#include <hal/nrf_vpr.h>
+#endif
 
 void main(void)
 {
+#if defined(CONFIG_BOARD_NRF54H20DK_NRF54H20_CPUAPP) && defined(CONFIG_ML_APP_INCLUDE_REMOTE_IMAGE)
+	/* Enable PPr core */
+	nrf_vpr_initpc_set(NRF_VPR130, PPR_START_ADDR);
+	nrf_vpr_cpurun_set(NRF_VPR130, true);
+#endif
+
 	if (app_event_manager_init()) {
 		LOG_ERR("Application Event Manager initialization failed");
 	} else {
