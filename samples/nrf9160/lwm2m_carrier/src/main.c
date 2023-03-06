b37de3376a318f1273a1b8b8b57e5638fd16c50c
Kacper Radoszewski <kacper.radoszewski@nordicsemi.no>
2023-03-06 15:39:19 +0100

    samples: lwm2m_carrier: update sample
    Updated the LwM2M Carrier sample.
    
    Signed-off-by: Kacper Radoszewski <kacper.radoszewski@nordicsemi.no>

--- samples/nrf9160/lwm2m_carrier/src/main.c
+++ samples/nrf9160/lwm2m_carrier/src/main.c
@@ -4,18 +4,66 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
+#include <zephyr/kernel.h>
+
 #ifdef CONFIG_LWM2M_CARRIER
 #include <lwm2m_carrier.h>
 #include "carrier_certs.h"
-#endif /* CONFIG_LWM2M_CARRIER */
-#include <zephyr/kernel.h>
 #include <modem/lte_lc.h>
+#include <modem/nrf_modem_lib.h>
 
-#ifdef CONFIG_LWM2M_CARRIER
+NRF_MODEM_LIB_ON_INIT(main_init_hook, on_modem_lib_init, NULL);
+
 static void lte_event_handler(const struct lte_lc_evt *const evt)
 {
 	/* This event handler is not in use here. */
 	ARG_UNUSED(evt);
+}
+
+static void on_modem_lib_init(int ret, void *ctx)
+{
+	int err;
+	static bool m_first_init = true;
+
+	ARG_UNUSED(ctx);
+
+	if (ret != 0) {
+		return;
+	}
+
+	/* Let the application write the credentials first and then bring the link up. */
+	if (m_first_init) {
+		err = carrier_cert_provision();
+		if (err) {
+			printk("failed to provision CA certificates\n");
+		}
+
+#ifdef LWM2M_CARRIER_SETTINGS
+		if (!lwm2m_settings_enable_custom_server_config_get()) {
+			err = carrier_psk_provision();
+			if (err) {
+				printk("failed to provision PSK\n");
+			}
+		}
+#else
+		err = carrier_psk_provision();
+		if (err) {
+			printk("failed to provision PSK\n");
+		}
+#endif /* LWM2M_CARRIER_SETTINGS */
+
+		lte_lc_register_handler(lte_event_handler);
+
+		m_first_init = false;
+
+		if (IS_ENABLED(CONFIG_LTE_AUTO_INIT_AND_CONNECT)) {
+			return;
+		}
+	}
+
+	/* LTE Link Controller is uninitialized on every modem shutdown. */
+	lte_lc_init();
+	lte_lc_connect_async(NULL);
 }
 
 void print_err(const lwm2m_carrier_event_t *evt)
@@ -37,8 +85,6 @@ void print_err(const lwm2m_carrier_event_t *evt)
 			"Illegal object configuration detected",
 		[LWM2M_CARRIER_ERROR_INIT] =
 			"Initialization failure",
-		[LWM2M_CARRIER_ERROR_INTERNAL] =
-			"Internal failure",
 		[LWM2M_CARRIER_ERROR_RUN] =
 			"Configuration failure",
 	};
@@ -88,31 +134,6 @@ int lwm2m_carrier_event_handler(const lwm2m_carrier_event_t *event)
 	int err = 0;
 
 	switch (event->type) {
-	case LWM2M_CARRIER_EVENT_INIT:
-		printk("LWM2M_CARRIER_EVENT_INIT\n");
-		err = carrier_cert_provision();
-		if (err) {
-			printk("failed to provision CA certificates\n");
-		}
-#ifdef LWM2M_CARRIER_SETTINGS
-		if (!lwm2m_settings_enable_custom_server_config_get()) {
-			err = carrier_psk_provision();
-			if (err) {
-				printk("failed to provision PSK\n");
-			}
-		}
-#else
-		err = carrier_psk_provision();
-		if (err) {
-			printk("failed to provision PSK\n");
-		}
-#endif /* LWM2M_CARRIER_SETTINGS */
-		err = lte_lc_init();
-		if (err != 0) {
-			printk("failed to initialize lte link controller library\n");
-		}
-		lte_lc_register_handler(lte_event_handler);
-		break;
 	case LWM2M_CARRIER_EVENT_LTE_LINK_UP:
 		printk("LWM2M_CARRIER_EVENT_LTE_LINK_UP\n");
 		err = lte_lc_connect_async(NULL);
@@ -154,4 +175,10 @@ int lwm2m_carrier_event_handler(const lwm2m_carrier_event_t *event)
 void main(void)
 {
 	printk("LWM2M Carrier library sample.\n");
+
+#ifdef CONFIG_LWM2M_CARRIER
+	if (!IS_ENABLED(CONFIG_NRF_MODEM_LIB_SYS_INIT)) {
+		nrf_modem_lib_init(NORMAL_MODE);
+	}
+#endif /* CONFIG_LWM2M_CARRIER */
 }
