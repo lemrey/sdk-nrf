d0255bc62292c99da4190262fa0330719908ac75
Marcin Jelinski <marcin.jelinski@nordicsemi.no>
2023-03-31 11:26:53 +0200

    samples: peripheral: radio_test: Refactor the GPPI API usage
    This commit implements the usage of the GPPI API in a consistent manner
    across various platforms.
    
    Ref: NCSDK-20228
    
    Signed-off-by: Marcin Jelinski <marcin.jelinski@nordicsemi.no>

--- samples/peripheral/radio_test/src/radio_test.c
+++ samples/peripheral/radio_test/src/radio_test.c
@@ -156,7 +156,6 @@ static void radio_power_set(nrf_radio_mode_t mode, uint8_t channel, int8_t power
 	}
 }
 
-#if defined(CONFIG_SOC_SERIES_NRF54HX)
 static void endpoints_clear(void)
 {
 	if (atomic_test_and_clear_bit(&endpoint_state, ENDPOINT_FORK_EGU_TIMER)) {
@@ -168,7 +167,7 @@ static void endpoints_clear(void)
 			nrf_egu_event_address_get(RADIO_TEST_EGU, RADIO_TEST_EGU_EVENT),
 			nrf_radio_task_address_get(NRF_RADIO, NRF_RADIO_TASK_TXEN) +
 							    SUBSCRIBE_EXTRA_OFFSET);
-	} 
+	}
 	if (atomic_test_and_clear_bit(&endpoint_state, ENDPOINT_EGU_RADIO_RX)) {
 		nrfx_gppi_channel_endpoints_clear(ppi_radio_start,
 			nrf_egu_event_address_get(RADIO_TEST_EGU, RADIO_TEST_EGU_EVENT),
@@ -181,15 +180,11 @@ static void endpoints_clear(void)
 			nrf_radio_task_address_get(NRF_RADIO, NRF_RADIO_TASK_TXEN) +
 							    SUBSCRIBE_EXTRA_OFFSET);
 	}
-
 }
-#endif /* defined(CONFIG_SOC_SERIES_NRF54HX) */
 
 static void radio_ppi_config(bool rx)
 {
-#if defined(CONFIG_SOC_SERIES_NRF54HX)
 	endpoints_clear();
-#endif /* defined(CONFIG_SOC_SERIES_NRF54HX) */
 
 	nrfx_gppi_channel_endpoints_setup(ppi_radio_start,
 			nrf_egu_event_address_get(RADIO_TEST_EGU, RADIO_TEST_EGU_EVENT),
@@ -211,7 +206,6 @@ static void radio_ppi_tx_reconfigure(void)
 		nrfx_gppi_channels_disable(BIT(ppi_radio_start));
 	}
 
-#if defined(CONFIG_SOC_SERIES_NRF54HX)
 	endpoints_clear();
 
 	nrfx_gppi_channel_endpoints_setup(ppi_radio_start,
@@ -219,14 +213,6 @@ static void radio_ppi_tx_reconfigure(void)
 		nrf_radio_task_address_get(NRF_RADIO, NRF_RADIO_TASK_TXEN) +
 						    SUBSCRIBE_EXTRA_OFFSET);
 	atomic_set_bit(&endpoint_state, ENDPOINT_TIMER_RADIO_TX);
-#else
-	nrfx_gppi_fork_endpoint_clear(ppi_radio_start,
-				nrf_timer_task_address_get(timer.p_reg, NRF_TIMER_TASK_START));
-	nrfx_gppi_event_endpoint_clear(ppi_radio_start,
-				nrf_egu_event_address_get(RADIO_TEST_EGU, RADIO_TEST_EGU_EVENT));
-	nrfx_gppi_event_endpoint_setup(ppi_radio_start,
-				nrf_timer_event_address_get(timer.p_reg, NRF_TIMER_EVENT_COMPARE1));
-#endif /* defined(CONFIG_SOC_SERIES_NRF54HX) */
 
 	nrfx_gppi_channels_enable(BIT(ppi_radio_start));
 }
@@ -746,21 +732,7 @@ void radio_test_cancel(void)
 		nrfx_gppi_channels_disable(BIT(ppi_radio_start));
 	}
 
-#if defined(CONFIG_SOC_SERIES_NRF54HX)
 	endpoints_clear();
-#else
-	nrfx_gppi_event_endpoint_clear(ppi_radio_start,
-			nrf_egu_event_address_get(RADIO_TEST_EGU, RADIO_TEST_EGU_EVENT));
-	nrfx_gppi_task_endpoint_clear(ppi_radio_start,
-			nrf_radio_task_address_get(NRF_RADIO, NRF_RADIO_TASK_TXEN));
-	nrfx_gppi_task_endpoint_clear(ppi_radio_start,
-			nrf_radio_task_address_get(NRF_RADIO, NRF_RADIO_TASK_RXEN));
-	nrfx_gppi_fork_endpoint_clear(ppi_radio_start,
-			nrf_timer_task_address_get(timer.p_reg, NRF_TIMER_TASK_START));
-	nrfx_gppi_event_endpoint_clear(ppi_radio_start,
-		nrf_timer_event_address_get(timer.p_reg, NRF_TIMER_EVENT_COMPARE1));
-#endif /* defined(CONFIG_SOC_SERIES_NRF54HX) */
-
 	radio_disable();
 }
 
