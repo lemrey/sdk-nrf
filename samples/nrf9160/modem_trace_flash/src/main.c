6697fce811542e42531c0a2aebbdea8be830ff2d
Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>
2023-05-10 12:17:24 +0200

    samples: nrf9160: modem_trace_flash: replace deprecated LTE_LC_ON_INIT
    Replace the deprecated LTE_LC_ON_INIT macro with AT_CMD_HOOK.
    
    Signed-off-by: Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>

--- samples/nrf9160/modem_trace_flash/src/main.c
+++ samples/nrf9160/modem_trace_flash/src/main.c
@@ -4,10 +4,13 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
+#include <stdlib.h>
+#include <string.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/uart.h>
+#include <modem/at_cmd_hook.h>
 #include <modem/lte_lc.h>
 #include <modem/nrf_modem_lib.h>
 #include <modem/nrf_modem_lib_trace.h>
@@ -25,13 +28,29 @@ LOG_MODULE_REGISTER(modem_trace_flash_sample, CONFIG_MODEM_TRACE_FLASH_SAMPLE_LO
 
 static const struct device *const uart_dev = DEVICE_DT_GET(UART1_DT_NODE);
 
-LTE_LC_ON_CFUN(cfun_hook, on_cfun, NULL);
-
-/* Callback for when modem functional mode is changed */
-static void on_cfun(enum lte_lc_func_mode mode, void *context)
+#if defined(CONFIG_AT_CMD_HOOK)
+static void pdn_cfun_evt(const char *cmd, int err)
 {
-	LOG_INF("LTE mode changed to %d\n", mode);
+	int mode;
+	char *mode_str;
+
+	if (err) {
+		return;
+	}
+
+	mode_str = (char *)cmd + strlen("AT+CFUN=");
+
+	if (mode_str[0] < '0' || mode_str[0] > '9') {
+		return;
+	}
+
+	mode = atoi(mode_str);
+
+	printk("LTE mode changed to %d\n", mode);
 }
+
+AT_CMD_HOOK(pdn_athook_cfun, "AT+CFUN=", NULL, pdn_cfun_evt);
+#endif /* AT_CMD_HOOK */
 
 static void print_uart1(char *buf, int len)
 {
