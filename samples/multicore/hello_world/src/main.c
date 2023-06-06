5a10ba77e911a2506cf73b80246a90c7f6322be1
Emil Obalski <Emil.Obalski@nordicsemi.no>
2023-06-06 11:12:24 +0200

    samples: multicore_hello: Execute PPR from RAM
    This enables cpuppr execution from ram.
    
    Signed-off-by: Emil Obalski <Emil.Obalski@nordicsemi.no>
    Signed-off-by: Mateusz.Michalek <Mateusz.Michalek@nordicsemi.no>

--- samples/multicore/hello_world/src/main.c
+++ samples/multicore/hello_world/src/main.c
@@ -9,20 +9,10 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(app);
 
-#if defined(CONFIG_BOARD_NRF54H20DK_NRF54H20_CPUAPP)
-#define PPR_START_ADDR DT_REG_ADDR(DT_NODELABEL(ppr_code))
-#include <hal/nrf_vpr.h>
-#endif
 
 int main(void)
 {
 	int cnt = 0;
-
-#if defined(CONFIG_BOARD_NRF54H20DK_NRF54H20_CPUAPP)
-	/* Enable PPr core */
-	nrf_vpr_initpc_set(NRF_VPR130, PPR_START_ADDR);
-	nrf_vpr_cpurun_set(NRF_VPR130, true);
-#endif
 
 	while (1) {
 		LOG_INF("test %d", cnt++);
