819ae290dae47e0db61f35b55b44de4f3141ddf7
Mateusz Michalek <mateusz.michalek@nordicsemi.no>
2023-06-22 15:17:27 +0200

    samples: caf_sensor_manager PPR auto-starting
    Removed cpuppr starting from cpuapp's main.c
    VPR loader is used instead.
    
    Signed-off-by: Mateusz Michalek <mateusz.michalek@nordicsemi.no>

--- samples/caf_sensor_manager/src/main.c
+++ samples/caf_sensor_manager/src/main.c
@@ -16,20 +16,8 @@ LOG_MODULE_REGISTER(MODULE);
 
 #include "event_proxy_init.h"
 
-#if defined(CONFIG_BOARD_NRF54H20DK_NRF54H20_CPUAPP) && \
-	defined(CONFIG_APP_INCLUDE_REMOTE_IMAGE)
-#define PPR_START_ADDR DT_REG_ADDR(DT_NODELABEL(ppr_code))
-#include <hal/nrf_vpr.h>
-#endif
-
 void main(void)
 {
-#if defined(CONFIG_BOARD_NRF54H20DK_NRF54H20_CPUAPP) && \
-	defined(CONFIG_APP_INCLUDE_REMOTE_IMAGE)
-	/* Enable PPr core */
-	nrf_vpr_initpc_set(NRF_VPR130, PPR_START_ADDR);
-	nrf_vpr_cpurun_set(NRF_VPR130, true);
-#endif
 	int ret;
 
 	ret = app_event_manager_init();
