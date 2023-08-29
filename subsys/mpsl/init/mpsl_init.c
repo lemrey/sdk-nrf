4e4188d9ea5ec8b5406e40a6624e1666fef7d8bc
Herman Berget <herman.berget@nordicsemi.no>
2023-08-29 14:22:45 +0200

    mpsl: Do not trigger clock calibration for nrf54h
    SYSCTRL handles the clock
    
    Signed-off-by: Herman Berget <herman.berget@nordicsemi.no>

--- subsys/mpsl/init/mpsl_init.c
+++ subsys/mpsl/init/mpsl_init.c
@@ -28,10 +28,10 @@ LOG_MODULE_REGISTER(mpsl_init, CONFIG_MPSL_LOG_LEVEL);
  */
 const uint32_t z_mpsl_used_nrf_ppi_channels = MPSL_RESERVED_PPI_CHANNELS;
 const uint32_t z_mpsl_used_nrf_ppi_groups;
-#if defined(CONFIG_CLOCK_CONTROL_NRF_K32SRC_RC)
+#if defined(CONFIG_CLOCK_CONTROL_NRF_K32SRC_RC) && !defined(CONFIG_SOC_SERIES_NRF54HX)
 static void mpsl_calibration_work_handler(struct k_work *work);
 static K_WORK_DELAYABLE_DEFINE(calibration_work, mpsl_calibration_work_handler);
-#endif /* CONFIG_CLOCK_CONTROL_NRF_K32SRC_RC */
+#endif /* CONFIG_CLOCK_CONTROL_NRF_K32SRC_RC && !CONFIG_SOC_SERIES_NRF54HX */
 
 #if IS_ENABLED(CONFIG_SOC_COMPATIBLE_NRF52X)
 	#define MPSL_LOW_PRIO_IRQn SWI5_IRQn
@@ -296,7 +296,7 @@ static uint8_t m_config_clock_source_get(void)
 }
 #endif /* !IS_ENABLED(CONFIG_SOC_PLATFORM_HALTIUM) */
 
-#if defined(CONFIG_CLOCK_CONTROL_NRF_K32SRC_RC)
+#if defined(CONFIG_CLOCK_CONTROL_NRF_K32SRC_RC) && !defined(CONFIG_SOC_SERIES_NRF54HX)
 static void mpsl_calibration_work_handler(struct k_work *work)
 {
 	ARG_UNUSED(work);
@@ -306,7 +306,7 @@ static void mpsl_calibration_work_handler(struct k_work *work)
 	k_work_schedule_for_queue(&mpsl_work_q, &calibration_work,
 				  K_MSEC(CONFIG_CLOCK_CONTROL_NRF_CALIBRATION_PERIOD));
 }
-#endif /* CONFIG_CLOCK_CONTROL_NRF_K32SRC_RC */
+#endif /* CONFIG_CLOCK_CONTROL_NRF_K32SRC_RC && !CONFIG_SOC_SERIES_NRF54HX */
 
 static int32_t mpsl_lib_init_internal(void)
 {
@@ -409,10 +409,10 @@ static int mpsl_low_prio_init(void)
 	IRQ_CONNECT(MPSL_LOW_PRIO_IRQn, MPSL_LOW_PRIO,
 		    mpsl_low_prio_irq_handler, NULL, 0);
 
-#if defined(CONFIG_CLOCK_CONTROL_NRF_K32SRC_RC)
+#if defined(CONFIG_CLOCK_CONTROL_NRF_K32SRC_RC) && !defined(CONFIG_SOC_SERIES_NRF54HX)
 	k_work_schedule_for_queue(&mpsl_work_q, &calibration_work,
 				  K_MSEC(CONFIG_CLOCK_CONTROL_NRF_CALIBRATION_PERIOD));
-#endif /* CONFIG_CLOCK_CONTROL_NRF_K32SRC_RC */
+#endif /* CONFIG_CLOCK_CONTROL_NRF_K32SRC_RC && !CONFIG_SOC_SERIES_NRF54HX */
 
 	return 0;
 }
