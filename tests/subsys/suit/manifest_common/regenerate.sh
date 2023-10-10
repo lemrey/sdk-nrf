0e12abd3f779cdee981a526b155940a590cbe6de
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2023-10-10 13:16:54 +0200

    tests: suit: Align regenerate.sh with generator
    Align the regenerate.sh script with the new suit-generator revision.
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- tests/subsys/suit/manifest_common/regenerate.sh
+++ tests/subsys/suit/manifest_common/regenerate.sh
@@ -7,6 +7,11 @@
 
 generated_files=()
 
+SUIT_PROCESSOR_DIR="../../../../../modules/lib/suit-processor"
+SUIT_GENERATOR_DIR="../../../../../modules/lib/suit-generator"
+SIGN_SCRIPT=${SUIT_GENERATOR_DIR}/ncs/sign_script.py
+KEYS_DIR=${SUIT_GENERATOR_DIR}/ncs
+
 if [ -z "$1" ]
   then
     echo "regenerate.sh: path to yaml file required"
@@ -14,28 +19,32 @@ if [ -z "$1" ]
 fi
 
 ### Key generation ###
-if [ ! -f key_priv.pem ]; then
+pushd ${KEYS_DIR} 1>/dev/null 2>/dev/null
+if [ ! -f key_private.pem ]; then
   echo "Generating private and public keys..."
   rm key_public.c
   suit-generator keys --output-file key
-  generated_files+=("\tprivate key:\t\t$PWD/key_priv.pem")
-  generated_files+=("\tpublic key:\t\t$PWD/key_pub.pem")
+  mv key_priv.pem key_private.pem
+  mv key_pub.pem key_public.pem
+  generated_files+=("\tprivate key:\t\t$PWD/key_private.pem")
+  generated_files+=("\tpublic key:\t\t$PWD/key_public.pem")
 fi
 if [ ! -f key_public.c ]; then
   echo "Generating public key as C source file..."
-  suit-generator convert --input-file key_priv.pem --output-file key_public.c
+  suit-generator convert --input-file key_private.pem --output-file key_public.c
   generated_files+=("\tpublic key C file:\t$PWD/key_public.c")
 fi
+popd 1>/dev/null 2>/dev/null
 
 ### Manifest generation ###
 echo "Generating SUIT envelope for $1 input file ..."
 suit-generator create --input-file $1 --output-file sample.suit
 generated_files+=("\tunsigned binary envelope:\t\t$PWD/sample.suit")
 echo "Signing SUIT envelope using key_priv.pem ..."
-suit-generator sign --input-file sample.suit --private-key key_priv.pem --output-file sample_signed.suit
+python3 ${SIGN_SCRIPT} --input-file sample.suit --output-file sample_signed.suit
 generated_files+=("\tsigned binary envelope:\t\t\t$PWD/sample_signed.suit")
 echo "Converting binary envelope into C code ..."
-zcbor convert -c ../../../../../modules/lib/suit-processor/cddl/manifest.cddl -c ../../../../../modules/lib/suit-processor/cddl/trust_domains.cddl -i ./sample_signed.suit --input-as cbor -t SUIT_Envelope_Tagged -o sample_signed.suit.c --c-code-var-name manifest --c-code-columns 15
+zcbor convert -c ${SUIT_PROCESSOR_DIR}/cddl/manifest.cddl -c ${SUIT_PROCESSOR_DIR}/cddl/trust_domains.cddl -i ./sample_signed.suit --input-as cbor -t SUIT_Envelope_Tagged -o sample_signed.suit.c --c-code-var-name manifest --c-code-columns 15
 generated_files+=("\tsigned binary envelope as C code:\t$PWD/sample_signed.suit.c")
 echo "Done, following files have been created:"
 for file in "${generated_files[@]}"
