4b8e772ae960ae9c0a4f34466c51ff11535cb7fe
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2023-10-18 16:57:58 +0200

    tests: suit: Change MCI in orchestrator test
    Use MCI implementation, dedicated for the SUIT test suites.
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- tests/subsys/suit/orchestrator/src/main.c
+++ tests/subsys/suit/orchestrator/src/main.c
@@ -16,26 +16,13 @@
 #include <update_magic_values.h>
 #include <suit_cache.h>
 
-#include <psa/crypto.h>
-
 #define SUIT_STORAGE_ADDRESS suit_plat_get_nvm_ptr(SUIT_STORAGE_OFFSET)
 #define SUIT_STORAGE_OFFSET  FIXED_PARTITION_OFFSET(suit_storage)
 #define SUIT_STORAGE_SIZE    FIXED_PARTITION_SIZE(suit_storage)
 
-#define MAX_PSA_KEY_COUNT  128
-#define PSA_DEFAULT_KEY_ID 0x7fffffe0
-
-#ifdef CONFIG_SOC_NRF54H20
-/** The value of UUID5("nRF54H20_sample_app"). */
-const suit_manifest_class_id_t supported_class_id = {{0x08, 0xc1, 0xb5, 0x99, 0x55, 0xe8, 0x5f,
-						      0xbc, 0x9e, 0x76, 0x7b, 0xc2, 0x9c, 0xe1,
-						      0xb0, 0x4d}};
-#else  /* CONFIG_SOC_NRF54H20 */
-/* The value of UUID5("posix_sample_app"). */
-const suit_manifest_class_id_t supported_class_id = {{0x56, 0xdc, 0x9a, 0x14, 0x28, 0xd8, 0x52,
-						      0xd3, 0xbd, 0x62, 0xe7, 0x7a, 0x08, 0xbc,
-						      0x8b, 0x91}};
-#endif /* CONFIG_SOC_NRF54H20 */
+const suit_manifest_class_id_t supported_class_id = {{0x5b, 0x46, 0x9f, 0xd1, 0x90, 0xee, 0x53,
+						      0x9c, 0xa3, 0x18, 0x68, 0x1b, 0x03, 0x69,
+						      0x5e, 0x36}};
 
 /* Valid envelope */
 extern const uint8_t manifest_valid_buf[];
@@ -90,7 +77,10 @@ static void setup_update_candidate(const uint8_t *buf, size_t len)
 
 	setup_erased_flash();
 
-	int err = mci_get_supported_manifest_class_ids(
+	int err = mci_init();
+	zassert_equal(0, err, "Failed to initialize MCI (%d)", err);
+
+	err = mci_get_supported_manifest_class_ids(
 		(const suit_manifest_class_id_t **)&supported_class_ids, &supported_class_ids_len);
 	zassert_equal(0, err, "Failed to get list of supported manifest class IDs (%d)", err);
 
@@ -102,59 +92,6 @@ static void setup_update_candidate(const uint8_t *buf, size_t len)
 		      len);
 }
 
-/* For storing and counting psa keys that were imported */
-static psa_key_id_t psa_key_ids[MAX_PSA_KEY_COUNT];
-static uint8_t psa_keys_count = 0;
-
-static void setup_use_all_available_psa_key_slots(void)
-{
-	zassert_equal(PSA_SUCCESS, psa_crypto_init(), "Cannot initialize psa crypto");
-
-	/* Public key to be imported */
-	const uint8_t public_key[] = {
-		0x04, /* POINT_CONVERSION_UNCOMPRESSED */
-		0xed, 0xd0, 0x9e, 0xa5, 0xec, 0xe4, 0xed, 0xbe, 0x6c, 0x08, 0xe7, 0x47, 0x09,
-		0x55, 0x9a, 0x38, 0x29, 0xc5, 0x31, 0x33, 0x22, 0x7b, 0xf4, 0xf0, 0x11, 0x6e,
-		0x8c, 0x05, 0x2d, 0x02, 0x0e, 0x0e, 0xc3, 0xe0, 0xd8, 0x37, 0xf4, 0xc2, 0x6f,
-		0xc1, 0x28, 0x80, 0x2f, 0x45, 0x38, 0x1a, 0x23, 0x2b, 0x6d, 0xd5, 0xda, 0x28,
-		0x60, 0x00, 0x5d, 0xab, 0xe2, 0xa0, 0x83, 0xdb, 0xef, 0x38, 0x55, 0x13};
-	/* Key attributes to be set */
-	psa_key_attributes_t key_attributes = PSA_KEY_ATTRIBUTES_INIT;
-	psa_set_key_usage_flags(&key_attributes, PSA_KEY_USAGE_VERIFY_HASH);
-	psa_set_key_algorithm(&key_attributes, PSA_ALG_ECDSA(PSA_ALG_SHA_256));
-	psa_set_key_type(&key_attributes, PSA_KEY_TYPE_ECC_PUBLIC_KEY(PSA_ECC_FAMILY_SECP_R1));
-
-	/* Clear key ids */
-	memset(psa_key_ids, 0, sizeof(psa_key_ids));
-
-	for (psa_keys_count = 0; psa_keys_count < MAX_PSA_KEY_COUNT; psa_keys_count++) {
-		/* Keep importing keys until all available slots are used */
-		if (PSA_SUCCESS != psa_import_key(&key_attributes, public_key, sizeof(public_key),
-						  &psa_key_ids[psa_keys_count])) {
-			break;
-		}
-	}
-
-	printk("Imported %d keys\n", psa_keys_count);
-}
-
-static void teardown_release_all_used_psa_key_slots(void)
-{
-	printk("Keys to destroy: %d\n", psa_keys_count);
-
-	for (; psa_keys_count > 0; psa_keys_count--) {
-		/* Skip the key ID that will be destroyed by the *after* function. */
-		if (psa_key_ids[psa_keys_count - 1] == PSA_DEFAULT_KEY_ID) {
-			continue;
-		}
-		int err = psa_destroy_key((mbedtls_svc_key_id_t)psa_key_ids[psa_keys_count - 1]);
-		zassert_equal(PSA_SUCCESS, err, "Failed to destroy allocated key with index %d: %d",
-			      psa_keys_count - 1, err);
-	}
-
-	memset(psa_key_ids, 0, sizeof(psa_key_ids));
-}
-
 static void setup_store_update_candidate_data(void *data, size_t len)
 {
 	setup_erased_flash();
@@ -164,14 +101,7 @@ static void setup_store_update_candidate_data(void *data, size_t len)
 		      "Unable to write data to flash");
 }
 
-static void destroy_default_key(void *fixture)
-{
-	int err = psa_destroy_key((mbedtls_svc_key_id_t)PSA_DEFAULT_KEY_ID);
-	zassert_equal(PSA_SUCCESS, err, "Failed to destroy the default key %d: %d",
-		      PSA_DEFAULT_KEY_ID, err);
-}
-
-ZTEST_SUITE(orchestrator_tests, NULL, NULL, NULL, destroy_default_key, NULL);
+ZTEST_SUITE(orchestrator_tests, NULL, NULL, NULL, NULL, NULL);
 
 ZTEST(orchestrator_tests, test_boot_path_empty_storage)
 {
@@ -222,8 +152,6 @@ ZTEST(orchestrator_tests, test_boot_path_successful_boot)
 	zassert_equal(0, suit_orchestrator_init(), "Orchestrator not initialized first time");
 	/* ... and orchestrator is launched first time to perform successful update */
 	zassert_equal(0, suit_orchestrator_entry(), "Unsuccessful first orchestrator launch");
-	/* ... and the key installed during the first initialization is destroyed */
-	destroy_default_key(NULL);
 	/* ... and the bootable envelope is installed */
 	int err = suit_storage_installed_envelope_get(&supported_class_id, &addr_boot, &size_boot);
 	zassert_equal(0, err, "The envelope was not installed after successful update");
@@ -260,23 +188,6 @@ ZTEST(orchestrator_tests, test_update_path_size_zero)
 	zassert_equal(storage[1], 0, "Update candidate presence not cleared");
 	/* ... and orchestrator returns success */
 	zassert_equal(0, err, "Unexpected error code");
-}
-
-ZTEST(orchestrator_tests, test_init_cannot_import_key)
-{
-	/* GIVEN all available psa key slots are occupied */
-	setup_use_all_available_psa_key_slots();
-
-	/* Try to initialize orchestrator - failure on key import should happen */
-
-	/* WHEN orchestrator is initialized */
-	int err = suit_orchestrator_init();
-
-	/* THEN it should fail on importing psa key */
-	zassert_equal(PSA_ERROR_INSUFFICIENT_MEMORY, err, "Unexpected error code");
-
-	/* Release all used psa keys as a cleanup before other tests */
-	teardown_release_all_used_psa_key_slots();
 }
 
 ZTEST(orchestrator_tests, test_update_path_manipulated_envelope)
